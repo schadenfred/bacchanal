e5adb10368b3e4115f0362f37cc47f27e8bade22
schadenfred <fred.schoeneman@gmail.com>
2015-03-07 01:55:10 -0800

    first commit

---
+++ app/controllers/static_controller.rb
@@ -0,0 +1,19 @@
+class StaticController < ApplicationController
+  def about
+  end
+
+  def terms
+  end
+
+  def privacy
+  end
+
+  def help
+  end
+
+  def contact
+  end
+
+  def home
+  end
+end
