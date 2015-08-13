f0153fff9b562a6d96b298477383b8f0a8e2c366
schadenfred <fred.schoeneman@gmail.com>
2015-08-13 14:22:32 -0700

    switched off .rvmrc

--- test/models/producer_test.rb
+++ test/models/producer_test.rb
@@ -7,12 +7,14 @@ describe Producer do
   describe "db" do 
 
     specify "columns & types" do 
+      
       must_have_column(:name, :string)
       must_have_column(:slug, :string)
       must_have_column(:facets, :hstore)
     end
 
     specify "indexes" do
+      
       must_have_index(:name) 
       must_have_index(:slug) 
     end
