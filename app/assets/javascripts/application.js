b460fe378c3b830267ff2db549de1adc0964127f
schadenfred <fred.schoeneman@gmail.com>
2015-08-17 14:51:41 -0700

    fixed template styles to reference assets or, in case of gmaps, public files

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -8,7 +8,7 @@
 //= require appear
 //= require jquery.simple-text-rotator.min
 //= require jqBootstrapValidation
-// require gmaps
+//= require gmaps
 //= require isotope.pkgd.min
 //= require imagesloaded.pkgd
 //= require jquery.flexslider-min
