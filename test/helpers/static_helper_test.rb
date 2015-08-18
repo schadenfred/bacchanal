f72e45a1396ea7d968ba355e5698e479c233d430
schadenfred <fred.schoeneman@gmail.com>
2015-08-17 18:41:09 -0700

    refactored wine quote helper

--- test/helpers/static_helper_test.rb
+++ test/helpers/static_helper_test.rb
@@ -3,6 +3,6 @@ require "test_helper"
 describe StaticHelper do 
 
   it "wine_quotes" do 
-    wine_quotes.first[:author].must_equal "Isak Dinesen"
+    wine_quotes.first["author"].must_equal "Isak Dinesen"
   end
 end
