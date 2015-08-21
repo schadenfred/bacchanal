d09054e89350ad72d05aff38bf2cf6883a1aa0ee
schadenfred <fred.schoeneman@gmail.com>
2015-08-20 20:54:59 -0700

    changed to stock asset compilatiion from new app

--- config/application.rb
+++ config/application.rb
@@ -28,8 +28,8 @@ module Bacchanal
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
-    config.cache_store = :dalli_store
-    config.action_controller.perform_caching = true
+    # config.cache_store = :dalli_store
+    # config.action_controller.perform_caching = true
 
 
   end
