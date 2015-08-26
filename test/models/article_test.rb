3b0607ea380e4f8b491ef72b5453569c787dc590
schadenfred <fred.schoeneman@gmail.com>
2015-08-25 18:28:37 -0700

    making index template render articles properly

--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -4,9 +4,25 @@ describe Article do
 
   subject { Article }
 
+  describe "db" do 
+
+    specify "columns & types" do 
+
+      must_have_column(:author_id, :integer)
+      must_have_column(:content, :text)
+      must_have_column(:slug)
+      must_have_column(:title)
+    end
+
+    specify "indexes" do
+
+    end
+  end
+
   specify "associations" do 
 
     must_belong_to(:org)
+    must_belong_to(:author)
     must_have_many(:comments)
   end
 end
