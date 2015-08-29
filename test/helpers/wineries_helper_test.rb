99ec8fea4aae47a6537625a2ebf3810a187bcab3
schadenfred <fred.schoeneman@gmail.com>
2015-08-29 00:49:04 -0700

    winerie tag helper finished

--- test/helpers/wineries_helper_test.rb
+++ test/helpers/wineries_helper_test.rb
@@ -3,6 +3,7 @@ require "test_helper"
 describe WineriesHelper do 
 
   it "#flavor_choices" do 
-    flavor_choices.must_equal "blah"
+    flavor_choices["spicy"]["spicy"].must_include "clove"
+    flavor_choices["fruity"]["citrus"].must_include "lemon"
   end
 end
