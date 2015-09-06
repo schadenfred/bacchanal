0dbd1b85def2af06cdd1afb2a0eacef471a8afc0
schadenfred <fred.schoeneman@gmail.com>
2015-09-06 12:13:02 -0700

    fixed auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,17 +3,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  http_basic_authenticate_with name: "bacchus", password: "godofwhine"
+  if Rails.env.production?
+    http_basic_authenticate_with name: "bacchus", password: "godofwhine"
+    end 
 
 
   # devise helper for authentication
   # before_action :authenticate_user!
-  # before_action :http_basic_authenticate!
-
-
-  # def http_basic_authenticate!
-  #   http_basic_authenticate_with(
-  #       name: "bacchus", password: "godofwhine"
-  #   ) if Rails.env.production?
-  # end
 end
