a511d6ed6d2e0a72a3df02100cf6fc654b066c52
schadenfred <fred.schoeneman@gmail.com>
2015-09-15 11:22:12 -0700

    cleaning up dashboard and views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,9 @@
 class ApplicationController < ActionController::Base
   
-  # While in development, and staging
+  # only allow testing by beta users, remove when ready for production
   before_action :authenticate_for_beta
-
   before_action :authenticate_user!
 
-  
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   # protect_from_forgery with: :exception
