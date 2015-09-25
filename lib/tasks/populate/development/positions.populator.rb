717479eb7c961724f20c711144f6d976eef0e74d
schadenfred <fred.schoeneman@gmail.com>
2015-09-24 20:59:40 -0700

    added positions populator

---
+++ lib/tasks/populate/development/positions.populator.rb
@@ -0,0 +1,28 @@
+
+positionables = %w[org]
+
+positionables.each do |positionable|
+
+  pm = positionable.classify.constantize
+
+  titles = %w[winemaker sales farmer owner labor bookkeeper]
+
+  pm.all[0..2].each do |object|
+
+    user_count = User.count
+
+    5.times do 
+
+      begin_date = (Date.today - rand(1..1000).days)
+      end_date = (begin_date + rand(1..6).years)
+      Position.create(
+        user_id: rand(1..user_count),
+        positionable_id: object.id,
+        positionable_type: object.class.name,
+        title: titles.sample,
+        tenure_start: begin_date,
+        tenure_end: end_date
+      )
+    end
+  end
+end
