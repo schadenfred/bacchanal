a9909f3a4ed141d76c2bdda4e9d3ac14d7193ae8
schadenfred <fred.schoeneman@gmail.com>
2015-11-05 15:59:33 -0800

    changed ssl to true

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,7 +21,9 @@ set :puma_error_log,  "#{release_path}/log/puma.access.log"
 set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
-set :puma_init_active_record, true  # Change to false when not using ActiveRecord
+set :puma_init_active_record, true  # false when not using ActiveRecord
+set :nginx_use_ssl, false
+
 
 ## Defaults:
 # set :scm,           :git
