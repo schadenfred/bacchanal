d3a8be98ee20d48d19bbbf49195cca1fd127795e
schadenfred <fred.schoeneman@gmail.com>
2015-11-10 14:29:15 -0800

    moved sample data into deidcated folder

---
+++ db/seeds/photos.seeds.rb
@@ -0,0 +1,14 @@
+# files = Dir[File.expand_path('test/samples/*.rb')]
+
+# image_path = Rails.root.join('test', 'samples', 'fanddcellars', 'sauv.jpg').to_s 
+
+# uploader = Dragonfly[:images] 
+# uploaded_image = uploader.fetch_file(image_path) 
+
+# image = Image.create image: uploaded_image 
+
+# if image.valid? 
+#   puts "#{image.title} Created" 
+# else 
+#   puts "Error on create #{image.title}" 
+# end
--- db/seeds/wineries.seeds.rb
+++ db/seeds/wineries.seeds.rb
@@ -1,6 +1,6 @@
 winery = Winery.create!(
   name: "Fathers & Daughters Cellars",
-  blog_title: nil,
+  blog_title: "Get some",
   mission: "Hand-crafted small batch wines from Ferrington Vineyard in the Anderson Valley, California.", 
   welcome_statement: "There are five of us. I’m one of the dads and I have two daughters, Taylor and Ella. My wife, Sarah, is the daughter of our little group’s patriarch, Kurt Schoeneman. Kurt is the owner of the Anderson Valley’s storied Ferrington Vineyard, the source of our fruit. Together, we make up Fathers & Daughters Cellars.
 
