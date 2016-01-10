d02780e39e352bf4e7b086ba28c2763571d78a51
schadenfred <fred.schoeneman@gmail.com>
2016-01-09 17:37:20 -0800

    rendered end and beginning ago

--- app/controllers/positions_controller.rb
+++ app/controllers/positions_controller.rb
@@ -3,16 +3,16 @@ class PositionsController < ApplicationController
   before_action :set_user, only: [:create]
 
   def create
-
     positionable_tokens = params[:position][:positionable_tokens].split(",")
 
     positionable_tokens.each do |token|      
       @user.positions.create(position_params.merge(positionable_id: token))
+
     end
 
     respond_to do |format|
       
-      format.html { redirect_to :back, notice: "positiions updated" }
+      format.html { redirect_to :dashboard, notice: "positions updated" }
       format.js
     end
 
@@ -25,7 +25,7 @@ class PositionsController < ApplicationController
     end
     
     def position_params
-      params.require(:position).permit(:title, :description, :user_id, :positionable_id, :positionable_type)
+      params.require(:position).permit(:title, :description, :user_id, :positionable_id, :positionable_type, :tenure_start, :tenure_end)
     end
 
     def set_user
--- test/integration/authentication/position_feature_test.rb
+++ test/integration/authentication/position_feature_test.rb
@@ -6,21 +6,18 @@ feature "Position Feature Test" do
   let(:winery) { create(:winery) } 
   
   scenario "add position to user and org" do 
-skip
+
     login_as user
     visit dashboard_path
 
     within("#positions") do 
       fill_in "Job or position title", with: "HMFIC"
-      fill_in "Winery, vineyard, or wine name", with: winery.name
+      fill_in "Organization", with: winery.id
       click_button "create a new position"
     end
-    user.positions.last.org.must_equal winery
-
-    # user.positions.last
-
-
-
+    
+    user.reload.positions.last.positionable.must_equal winery
+    page.assert_selector(".alert", text: /positions updated/)
   end
 end
 
