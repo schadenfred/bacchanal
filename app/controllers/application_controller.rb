a9c41a0c53b5e81ae61fc1c64f31b004b83050e6
schadenfred <fred.schoeneman@gmail.com>
2016-03-11 17:04:35 -0500

    modified after_sign_in_path using switch

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,10 +6,21 @@ class ApplicationController < ActionController::Base
 
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  # protect_from_forgery with: :exception
-
+  protect_from_forgery with: :exception
 
   include Pundit
+
+  def after_sign_in_path_for(resource)
+    case current_user.roles.first.name
+    when "user"
+      root_path
+
+    when "owner"
+      root_path
+    when "admin"
+      root_path
+    end
+  end
 
   private
 
